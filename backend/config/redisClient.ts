5afcafb815d261b1abd6e7d433debd2d79ad606a
amirali-rabiei <amiralirabiei22@gmail.com>
2025-07-09 17:26:33 +0330

    added redis server

--- backend/config/redisClient.ts
+++ backend/config/redisClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from "redis"
 
-export const redisClient = createClient()
+export const redisClient = createClient({
+    url: process.env.REDIS_URL
+})
 redisClient.on('error', (error) => console.error(error))
 
 export const connectRedis = async () => {
